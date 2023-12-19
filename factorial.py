c548f860f6624cdf82e1e55eea8fb0f9dcf1a203
radhapatel41 <154301059+radhapatel41@users.noreply.github.com>
2023-12-19 10:54:47 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,8 +1,6 @@
 n = 23
 fact = 1
- 
-for i in range(1, n+1):
+ for i in range(1, n+1):
     fact = fact * i
- 
-print("The factorial of 23 is : ", end="")
+ print("The factorial of 23 is : ", end="")
 print(fact)
